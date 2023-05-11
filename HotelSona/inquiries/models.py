ac8c3aff642f5c49c876b0666ac2fa05e25dd893
Kim Chan Ho <chgim1128@gmail.com>
2023-05-11 22:59:11 +0900

    chanho
    질문, 답변 작성

--- HotelSona/inquiries/models.py
+++ HotelSona/inquiries/models.py
@@ -4,13 +4,14 @@ from django.utils import timezone
 class Answer(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    
+   
+
     def __str__(self):
         return self.content
 
 class Question(models.Model):
-    author = models.CharField(max_length=100)
-    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=10)
+    title = models.CharField(max_length=50)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     answer = models.OneToOneField('Answer', on_delete=models.CASCADE, null=True, blank=True)
