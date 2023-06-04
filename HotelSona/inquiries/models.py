d76ad482d71184f1118469ca02d648c572dfd0ce
Kim Chan Ho <chgim1128@gmail.com>
2023-06-04 22:44:07 +0900

    찬호
    게시판 거의 완성 삭제 기능만 추가 하면 됨

--- HotelSona/inquiries/models.py
+++ HotelSona/inquiries/models.py
@@ -4,17 +4,17 @@ from django.utils import timezone
 class Answer(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-   
 
     def __str__(self):
         return self.content
 
 class Question(models.Model):
-    author = models.CharField(max_length=10)
+    author = models.CharField(max_length=30)
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     answer = models.OneToOneField('Answer', on_delete=models.CASCADE, null=True, blank=True)
+    is_public = models.BooleanField(default=True)  # 공개 여부를 나타내는 필드
 
     def __str__(self):
         return self.title
--- HotelSona/inquiries/views.py
+++ HotelSona/inquiries/views.py
@@ -7,12 +7,11 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 
 
-
 def customer_voice(request):
-    question_list = Question.objects.order_by('-created_date') # order the questions by created_date
-    paginator = Paginator(question_list, 5) # 10 questions per page
-    page = request.GET.get('page') # get the current page number from the request's query parameter
-    questions = paginator.get_page(page) # get the questions for the current page
+    question_list = Question.objects.order_by('-created_date')
+    paginator = Paginator(question_list, 5)
+    page = request.GET.get('page')
+    questions = paginator.get_page(page)
     context = {'questions': questions}
     return render(request, 'customer-voice.html', context)
 
@@ -23,13 +22,21 @@ def customer_question(request):
         author = request.POST.get('author')
         title = request.POST.get('title')
         content = request.POST.get('content')
-        created_date = datetime.now().strftime("%Y-%m-%d") # 현재 시간을 yyyy-mm-dd 형식으로 변경
-        question = Question.objects.create(author=author, title=title, content=content, created_date=created_date)
+        created_date = datetime.now().strftime("%Y-%m-%d")
+        is_public = request.POST.get('is_public') == 'True'
+        question = Question.objects.create(author=author, title=title, content=content, created_date=created_date, is_public=is_public)
         return redirect('inquiries:customer_voice')
     else:
         return render(request, 'customer-question.html')
 
 def question_detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    answer = question.answer
-    return render(request, 'question-detail.html', {'question': question, 'answer': answer})
+    if question.is_public:
+        answer = question.answer
+        return render(request, 'question-detail.html', {'question': question, 'answer': answer})
+    elif not question.is_public and question.author == request.user.username:
+        answer = question.answer
+        return render(request, 'question-detail.html', {'question': question, 'answer': answer})
+    else:
+        messages.error(request, '해당 질문은 비공개되어 있습니다.')
+        return redirect('inquiries:customer_voice')
