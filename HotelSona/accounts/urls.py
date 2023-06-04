561f3fbbcb00051d727bf9bc484e22a70ef5328a
Kim Chan Ho <chgim1128@gmail.com>
2023-06-04 17:24:10 +0900

    chan
    로그인 시스템 적용

--- HotelSona/accounts/urls.py
+++ HotelSona/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login_view, name='login_view'),
     path('register/', views.register_view, name='register_view'),
+    path('logout/', views.logout, name='logout'),  # 추가: logout URL 패턴
 ]
--- HotelSona/accounts/views.py
+++ HotelSona/accounts/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
+def register_view(request):
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            user = User.objects.create_user(
+                username=request.POST["email"], password=request.POST["password1"])
+            auth.login(request, user)
+            return redirect('booking:index')
+        return render(request, 'register.html')
+    
+    return render(request, 'register.html')
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            # 로그인 성공 시 추가 동작 수행
+            # return render(request, 'booking:index', {'success': '로그인이 성공했습니다.'})
+            return redirect('booking:index')
+        else:
+            return render(request, 'login.html', {'error': '이메일 또는 비밀번호가 올바르지 않습니다.'})
+    else:
+        return render(request, 'login.html')
 
-
-def register_view(request):
-    return render(request, 'register.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('booking:index')
