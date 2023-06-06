5d31c51d8b10286a46d23a2d2af3b4b44e34faee
Kim Chan Ho <chgim1128@gmail.com>
2023-06-06 21:03:43 +0900

    찬호
    여기까지 정상작동

--- HotelSona/booking/views.py
+++ HotelSona/booking/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def about_us(request):
     return render(request, 'about-us.html')
 
-
+@login_required
 def booking_2(request):
     return render(request, 'booking-2.html')
 
-
+@login_required
 def booking_3(request):
     return render(request, 'booking-3.html')
 
-
+@login_required
 def booking(request):
     return render(request, 'booking.html')
 
